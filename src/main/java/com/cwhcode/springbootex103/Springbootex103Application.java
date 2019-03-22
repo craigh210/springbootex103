d84fb1133227dfe89c2e9a117e4f25fab99aa90e
craigh210 <craighunt210@gmail.com>
2019-03-22 10:53:56 -0400

    Initial commit

---
+++ src/main/java/com/cwhcode/springbootex103/Springbootex103Application.java
@@ -0,0 +1,13 @@
+package com.cwhcode.springbootex103;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootex103Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springbootex103Application.class, args);
+    }
+
+}
