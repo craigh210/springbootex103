043cce43d10df38dddf442902bcbb5b0393c5f94
craigh210 <craighunt210@gmail.com>
2020-06-19 12:05:00 -0400

    Testing from Eclipse

--- src/main/java/com/cwhcode/springbootex103/HomeController.java
+++ src/main/java/com/cwhcode/springbootex103/HomeController.java
@@ -1,11 +1,14 @@
 package com.cwhcode.springbootex103;
 
+//Define the imports
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+//Use an annotation to indicate this is the controller
 @Controller
 public class HomeController {
-    @RequestMapping("/")
+    //Map the default home page to this method
+	@RequestMapping("/")
     public String homePage() {
         return "hometemplate";
     }
